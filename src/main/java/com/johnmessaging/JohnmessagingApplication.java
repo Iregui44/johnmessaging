21ad279375560a8646af6a486c3acb52821e0b64
Felipe Iregui <pireguis_ca64@hotmail.com>
2025-09-04 19:59:49 +0200

    Creat spring proyect

---
+++ src/main/java/com/johnmessaging/JohnmessagingApplication.java
@@ -0,0 +1,13 @@
+package com.johnmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnmessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohnmessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnmessaging/JohnmessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohnmessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
